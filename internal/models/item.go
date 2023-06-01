0a54716684c9a121c32325ee4fd7a0f234776d3f
Fajri Kornel <fajri.kornel@gopay.co.id>
2023-06-01 16:04:31 +0700

    feat: update item model

--- internal/models/item.go
+++ internal/models/item.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Item struct {
-	ID          uint   `json:"item_id"`
-	ProjectID   uint   `json:"-"`
+	ID          uint   `json:"item_id" gorm:"primaryKey;autoIncrement:true"`
+	ProjectID   uint   `json:"-" gorm:"primaryKey;autoIncrement:false"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	gorm.Model  `json:"-"`
